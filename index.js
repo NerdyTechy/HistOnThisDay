9b3f175f7167207cfb8cd0cb32d35f3527e3b3a9
NerdyTechy <techy@techy.lol>
2022-12-30 15:04:29 +0000

    Add eslint as a dev dependency

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ const client = new Twitter({
     access_token_secret: process.env.accessTokenSecret,
 });
 
-const job = schedule.scheduleJob("0 30 13 * * *", function start() {
+schedule.scheduleJob("0 30 13 * * *", function start() {
     run();
 });
 
 console.log("The bot is now ready and will post at 13:30 every day.");
 
 async function run() {
-    const events = await fetch(`https://byabbe.se/on-this-day/12/30/events.json`).then((data) => data.json());
+    const events = await fetch("https://byabbe.se/on-this-day/12/30/events.json").then((data) => data.json());
 
     var eventNo = Math.floor(Math.random() * events.events.length);
     while (events.events[eventNo].length >= 245) {
-        var eventNo = Math.floor(Math.random() * events.events.length);
+        eventNo = Math.floor(Math.random() * events.events.length);
     }
 
     function post(callback) {
         var message = `#OnThisDay ${events.date}, ${events.events[eventNo].year}: ${events.events[eventNo].description}`;
 
-        client.post("statuses/update", { status: message, stringify_ids: true }, (err, tweet, res) => {
+        client.post("statuses/update", { status: message, stringify_ids: true }, (err, tweet) => {
             if (err) return console.error(err);
             callback(tweet.id_str);
         });
@@ -36,12 +36,8 @@ async function run() {
 
     post((lastTweetId) => {
         var message = `📖 Read More: ${events.events[eventNo].wikipedia[0].wikipedia}`;
-        client.post("statuses/update", { status: message, in_reply_to_status_id: lastTweetId }, (err, tweet, res) => {
+        client.post("statuses/update", { status: message, in_reply_to_status_id: lastTweetId }, (err) => {
             if (err) return console.error(err);
         });
     });
-
-    function buildTweet(message, first) {
-        return `${first ? `` : ``} ${message}`;
-    }
 }
